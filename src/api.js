0df05a7922249638f0b253d3bbf5cb635e6d222a
Trịnh Công Vũ <vu@MacBook-Pro-cua-Trinh.local>
2022-08-08 12:52:15 +0700

    alo

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoAPIOptions = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '4ec3f016c5mshabb2c9b9b607e75p1ecd18jsn3d16829500ff',
+            'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+        }
+    };
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+    
+    
